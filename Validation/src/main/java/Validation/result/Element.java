9c2d9b04d2236e0d50251543eefea28c65934c03
euclidgame <1500880529@qq.com>
2022-11-29 16:47:07 +0800

    add classes to process result

---
+++ Validation/src/main/java/Validation/result/Element.java
@@ -0,0 +1,33 @@
+package Validation.result;
+
+public class Element<T> {
+
+    private final T element;
+
+    private T representative;
+
+    private final int order;
+
+    public Element(T element, T representative, int order) {
+        this.element = element;
+        this.representative = representative;
+        this.order = order;
+    }
+
+    public T getElement() {
+        return element;
+    }
+
+    public void setRepresentative(T representative) {
+        this.representative = representative;
+    }
+
+    public T getRepresentative() {
+        return representative;
+    }
+
+    public int getOrder() {
+        return order;
+    }
+
+}
---
+++ Validation/src/main/java/Validation/result/ResultHolder.java
@@ -0,0 +1,52 @@
+package Validation.result;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
+public class ResultHolder<T> {
+
+    private final List<Element<T>> representatives = new ArrayList<>();
+
+    private final Map<T, Element<T>> elements = new HashMap<>();
+
+    private int count = 0;
+
+    public ResultHolder() {}
+
+    public void addRepresentative(T representative) {
+        Element<T> newElement = new Element<>(representative, representative, ++ count);
+        elements.put(representative, newElement);
+        representatives.add(newElement);
+    }
+
+    public void addElement(T element, T representative) {
+        Element<T> newElement = new Element<>(element, representative, ++ count);
+        elements.put(element, newElement);
+    }
+
+    public Stream<T> representatives() {
+        return representatives.stream().map(Element::getElement);
+    }
+
+    public List<T> getRepresentatives() {
+        List<T> ret = new ArrayList<>();
+        representatives.forEach(r -> ret.add(r.getElement()));
+        return ret;
+    }
+
+    public Stream<T> elements() {
+        return elements.keySet().stream();
+    }
+
+    public T getRepresentativeOf(T program) {
+        return elements.get(program).getRepresentative();
+    }
+
+    public int getOrderOf(T program) {
+        return elements.get(program).getOrder();
+    }
+
+}
